05b561f06554ed23060926ae6d27913a73f750fa
Samriddhi Paudel <paudelsamriddhi04@gmail.com>
2023-08-08 19:30:30 +0545

    Changed pictures for projects

--- src/app/sections/Projects/Projects.tsx
+++ src/app/sections/Projects/Projects.tsx
@@ -6,11 +6,14 @@ import ProjectCard from "./components/ProjectCard";
 
 const Projects = () => {
   const projects: Project[] = [
-    { title: "Residential", image: "/images/projects/residential.png" },
-    { title: "Commercial Complex", image: "/images/projects/residential.png" },
-    { title: "Sustainable Housing", image: "/images/projects/SustainableHousing.png" },
-    { title: "Office Spaces", image: "/images/projects/OfficeSpaces.png" },
-    { title: "Architectural Marvels", image: "/images/projects/ArchitecturalMarvels.png" },
+    { title: "Bhatbhateni Supermarket", image: "/images/projects/BhatbhateniProject.jpeg" },
+    { title: "Hotel De'l Annapurna", image: "/images/projects/HotelAnnapurna.jpg" },
+    { title: "Roadhshow Housing", image: "/images/projects/RoadhshowHousing.jpg" },
+    { title: "Bhatbhateni Apartment", image: "/images/projects/Apartment.jpg" },
+    { title: "Annapurna Cable Car", image: "/images/projects/AnnarpurnaCableCar.jpeg" },
+    { title: "Valley Homes", image: "/images/projects/ResidentialPic.jpg" },
+    { title: "Deep Housing, Pokhara", image: "/images/projects/DeepHousing1.jpg" },
+    
   ];
 
   return (
@@ -19,7 +22,7 @@ const Projects = () => {
         Our Recent <TextUnderline>Projects</TextUnderline>
       </Title>
 
-      <div className="lg:grid lg:grid-cols-4 lg:gap-5 lg:grid-rows-2 mx-auto px-6 lg:px-0 gap-10 mt-16 ">
+      <div className="lg:grid lg:grid-cols-4 lg:gap-5 lg:grid-rows-2 flex flex-col mx-auto px-6 lg:px-0 gap-10 mt-16 ">
         {/* {projects.map((project, i) => ( */}
           <div className="col-start-1 col-end-3 hover:shadow-[-8px_8px_0px_0px_rgba(0,0,0,0.94)] transition-all hover:shadow-primary rounded-3xl hover:translate-x-3 hover:-translate-y-1 mb-6 lg:mb-0 cursor-pointer">
             <ProjectCard project={projects[0]} />
@@ -36,6 +39,13 @@ const Projects = () => {
           <div className="col-start-3 col-end-5 hover:shadow-[-8px_8px_0px_0px_rgba(0,0,0,0.94)] transition-all hover:shadow-primary rounded-2xl hover:translate-x-3 hover:-translate-y-1 cursor-pointer">
             <ProjectCard project={projects[4]}/>
           </div>
+          <div className="col-start-1 col-end-3 hover:shadow-[-8px_8px_0px_0px_rgba(0,0,0,0.94)] transition-all hover:shadow-primary rounded-3xl hover:translate-x-3 hover:-translate-y-1 mb-6 lg:mb-0 cursor-pointer">
+            <ProjectCard project={projects[5]} />
+          </div>
+          <div className="col-start-3 col-end-5 hover:shadow-[-8px_8px_0px_0px_rgba(0,0,0,0.94)] transition-all hover:shadow-primary rounded-2xl hover:translate-x-3 hover:-translate-y-1 mb-6 lg:mb-0 cursor-pointer">
+            <ProjectCard project={projects[6]}/>
+          </div>
+
         {/* ))} */}
       </div>
     </Section>

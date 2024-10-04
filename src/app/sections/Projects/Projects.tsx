d97ec38ab725fbf24227e15c83de38f6b18efe24
AryalKTM <aryalbipashree@gmail.com>
2024-10-04 12:24:47 +0000

    Removed onHover Animations from Projects

--- src/app/sections/Projects/Projects.tsx
+++ src/app/sections/Projects/Projects.tsx
@@ -1,6 +1,4 @@
-import { Section, TextUnderline, Title } from "@/app/components";
-
-import Residential from "@/assets/images/residential.png";
+import { Section, Title } from "@/app/components";
 import { Project } from "@/domain/models/project";
 import ProjectCard from "./components/ProjectCard";
 
@@ -13,7 +11,6 @@ const Projects = () => {
     { title: "Annapurna Cable Car", image: "/images/projects/AnnarpurnaCableCar.jpeg" },
     { title: "Valley Homes", image: "/images/projects/ResidentialPic.jpg" },
     { title: "Deep Housing, Pokhara", image: "/images/projects/DeepHousing1.jpg" },
-    
   ];
 
   return (
@@ -23,30 +20,27 @@ const Projects = () => {
       </Title>
 
       <div className="lg:grid lg:grid-cols-4 lg:gap-5 lg:grid-rows-2 flex flex-col mx-auto px-6 lg:px-0 gap-10 mt-16 ">
-        {/* {projects.map((project, i) => ( */}
-          <div className="col-start-1 col-end-3 hover:shadow-[-8px_8px_0px_0px_rgba(0,0,0,0.94)] transition-all hover:shadow-primary rounded-3xl hover:translate-x-3 hover:-translate-y-1 mb-6 lg:mb-0 cursor-pointer">
-            <ProjectCard project={projects[0]} />
-          </div>
-          <div className="col-start-3 col-end-5 hover:shadow-[-8px_8px_0px_0px_rgba(0,0,0,0.94)] transition-all hover:shadow-primary rounded-2xl hover:translate-x-3 hover:-translate-y-1 mb-6 lg:mb-0 cursor-pointer">
-            <ProjectCard project={projects[1]}/>
-          </div>
-          <div className="col-start-1 col-end-2 hover:shadow-[-8px_8px_0px_0px_rgba(0,0,0,0.94)] transition-all hover:shadow-primary rounded-2xl hover:translate-x-3 hover:-translate-y-1 mb-6 lg:mb-0 cursor-pointer">
-            <ProjectCard project={projects[2]}/>
-          </div>
-          <div className="col-start-2 col-end-3 hover:shadow-[-8px_8px_0px_0px_rgba(0,0,0,0.94)] transition-all hover:shadow-primary rounded-2xl hover:translate-x-3 hover:-translate-y-1 mb-6 lg:mb-0 cursor-pointer">
-            <ProjectCard project={projects[3]}/>
-          </div>
-          <div className="col-start-3 col-end-5 hover:shadow-[-8px_8px_0px_0px_rgba(0,0,0,0.94)] transition-all hover:shadow-primary rounded-2xl hover:translate-x-3 hover:-translate-y-1 cursor-pointer">
-            <ProjectCard project={projects[4]}/>
-          </div>
-          <div className="col-start-1 col-end-3 hover:shadow-[-8px_8px_0px_0px_rgba(0,0,0,0.94)] transition-all hover:shadow-primary rounded-3xl hover:translate-x-3 hover:-translate-y-1 mb-6 lg:mb-0 cursor-pointer">
-            <ProjectCard project={projects[5]} />
-          </div>
-          <div className="col-start-3 col-end-5 hover:shadow-[-8px_8px_0px_0px_rgba(0,0,0,0.94)] transition-all hover:shadow-primary rounded-2xl hover:translate-x-3 hover:-translate-y-1 mb-6 lg:mb-0 cursor-pointer">
-            <ProjectCard project={projects[6]}/>
-          </div>
-
-        {/* ))} */}
+        <div className="col-start-1 col-end-3 mb-6 lg:mb-0 cursor-pointer">
+          <ProjectCard project={projects[0]} />
+        </div>
+        <div className="col-start-3 col-end-5 mb-6 lg:mb-0 cursor-pointer">
+          <ProjectCard project={projects[1]} />
+        </div>
+        <div className="col-start-1 col-end-2 mb-6 lg:mb-0 cursor-pointer">
+          <ProjectCard project={projects[2]} />
+        </div>
+        <div className="col-start-2 col-end-3 mb-6 lg:mb-0 cursor-pointer">
+          <ProjectCard project={projects[3]} />
+        </div>
+        <div className="col-start-3 col-end-5 mb-6 lg:mb-0 cursor-pointer">
+          <ProjectCard project={projects[4]} />
+        </div>
+        <div className="col-start-1 col-end-3 mb-6 lg:mb-0 cursor-pointer">
+          <ProjectCard project={projects[5]} />
+        </div>
+        <div className="col-start-3 col-end-5 mb-6 lg:mb-0 cursor-pointer">
+          <ProjectCard project={projects[6]} />
+        </div>
       </div>
     </Section>
   );
